239b10721c7dda8cea07b4e6d78e387ad6b4b4c3
Zac H <zac.haluza@gmail.com>
2021-04-26 00:02:07 -0400

    create oauth server

---
+++ oauthServer.ts
@@ -0,0 +1,13 @@
+import OAuthServer from 'express-oauth-server';
+import * as model from './models/dbModels';
+
+export default new OAuthServer({
+  // NOTE: oauth2-server and mongoose have conflicting types
+  //@ts-ignore:disable-next-line
+  model,
+  grants: ['authorizationCode', 'refreshToken'],
+  accessTokenLifetime: 60 * 60 * 24, // 1 day
+  allowEmptyState: true,
+  allowExtendedTokenAttributes: true,
+  debug: true,
+});
