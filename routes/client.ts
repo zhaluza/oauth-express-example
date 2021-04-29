73581a8880b453506d0df272bea6c22ec5ff2cc9
Zac H <zac.haluza@gmail.com>
2021-04-28 22:35:35 -0400

    add post route for creating client

--- routes/client.ts
+++ routes/client.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { Router } from 'express';
+import { createClient } from '../models/dbModels';
 
 const router = Router();
 router.get('/', (req, res) =>
@@ -9,5 +10,13 @@ router.get('/', (req, res) =>
 router.get('/app', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/clientApp.html'))
 );
+
+router.post('/', (req, res) => {
+  const { clientId, clientSecret, redirectUris, grants } = req.body;
+  if (!clientId || !clientSecret || !redirectUris || !grants)
+    return res.json({ message: 'Please include all client fields' });
+  createClient({ clientId, clientSecret, grants, redirectUris });
+  return res.json({ status: 'successful' });
+});
 
 export default router;
